7e1210a515423b0438d9b625d6a7118bab5f404a
Alexey <doom9936.1999@gmail.com>
2019-09-07 15:53:21 +0300

    Add HelloWorld UWP Project

---
+++ HelloWorld/MainPage.xaml.h
@@ -0,0 +1,23 @@
+﻿//
+// MainPage.xaml.h
+// Объявление класса MainPage.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace HelloWorld
+{
+	/// <summary>
+	/// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void ButtonClick(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
