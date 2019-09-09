71d00f9f54511617e71866a652f1be230c90f159
Alexey <doom9936.1999@gmail.com>
2019-09-09 15:04:20 +0300

    Add MainPage for power-split-2 Project

---
+++ power-split-2/MainPage.xaml.h
@@ -0,0 +1,22 @@
+﻿//
+// MainPage.xaml.h
+// Объявление класса MainPage.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace PowerSplit
+{
+	/// <summary>
+	/// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+	};
+}
