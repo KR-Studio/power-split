d0d9190715945bdf7f3d2dc1f09a6d665c172886
Yaroslav Rezaiev <yar.re.slav@gmail.com>
2019-09-10 18:46:30 +0300

    Update function prototype for TextBlock->Text value update
    (still non-operational)

--- power-split-2/MainPage.xaml.cpp
+++ power-split-2/MainPage.xaml.cpp
@@ -26,8 +26,8 @@ using namespace Windows::UI::Xaml::Shapes;
 
 std::mutex mtx;
 std::map<String^, String^> squares_map;
-//std::vector<TextBlock^> textblocks;
-TextBlock^ textblocks[4];
+std::map<String^, String^> textblocks_map;
+std::map<TextBlock^, String^> textblocks_map2;
 
 MainPage::MainPage()
 {
@@ -39,15 +39,10 @@ void thread_rect_square(String^ name, int width, int height) {
 	int squareNum = width * height;
 	std::wstring squareNumWstr = std::to_wstring(squareNum);
 	String^ squareNumPStr = ref new String(squareNumWstr.c_str());
-	String^ nameTextBlock = name + "Text";
-	//for (auto &textblock : textblocks) {
-		//if ((nameTextblock) == textblock->Name) {
-			//textblock->Text = "etttsstststt";
-		//}
-	//}
-	rectangle1Text->Text = "sheeeeesh!";
-	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	String^ textBlockText = name;
 	squares_map[name] = squareNumPStr;
+	textblocks_map.insert({ textBlockText, squareNumPStr });
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 void thread_all_rect_square() {
@@ -66,22 +61,31 @@ void thread_all_rect_square() {
 void PowerSplit::MainPage::RectangleTextLoaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
 	Rectangle^ rectangles[] = { rectangle1, rectangle2, rectangle3, rectangle4 };
-	//textblocks.push_back(rectangle1Text);
-	//textblocks.push_back(rectangle2Text);
-	//textblocks.push_back(rectangle3Text);
-	//textblocks.push_back(rectangle4Text);
-	textblocks[0] = rectangle1Text;
-	textblocks[1] = rectangle2Text;
-	textblocks[2] = rectangle3Text;
-	textblocks[3] = rectangle4Text;
+	textblocks_map2.insert({ rectangle1Text, rectangle1Text->Text });
+	textblocks_map2.insert({ rectangle2Text, rectangle2Text->Text });
+	textblocks_map2.insert({ rectangle3Text, rectangle3Text->Text });
+	textblocks_map2.insert({ rectangle4Text, rectangle4Text->Text });
+	/*textblocks[0] = rectangle1text;
+	textblocks[1] = rectangle2text;
+	textblocks[2] = rectangle3text;
+	textblocks[3] = rectangle4text;*/
+
 	std::vector<std::thread> threads;
 
 	for each (Rectangle^ rectangle in rectangles) {
 		std::thread thr(thread_rect_square, rectangle->Name ,rectangle->Width, rectangle->Height);
 		threads.emplace_back(std::move(thr));
 	}
+
+	for (auto& [key, val] : textblocks_map2) {
+		//if (textblocks_map.find(val) != textblocks_map.end) {
+			key->Text = textblocks_map.find(val)->second;
+		//}/
+	}
+
 	std::thread thr(thread_all_rect_square);
 	threads.emplace_back(std::move(thr));
+
 	for (auto& thread : threads) {
 		thread.join();
 	}
