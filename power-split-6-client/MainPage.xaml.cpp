78670dea63676664ad3d286776ed820abda50e8a
Alexey Kharchenko <doom9936.1999@gmail.com>
2019-10-05 16:08:56 +0300

    Add Connection and Disconnection for power-split-6 Projects

--- power-split-6-client/MainPage.xaml.cpp
+++ power-split-6-client/MainPage.xaml.cpp
@@ -32,18 +32,18 @@ MainPage::MainPage()
 	InitializeComponent();
 }
 
-std::wstring s2ws(const std::string& dataStr)
+std::wstring s2ws(const std::string& outputDataStr)
 {
-	int size_needed = MultiByteToWideChar(CP_UTF8, 0, &dataStr[0], (int)dataStr.size(), NULL, 0);
+	int size_needed = MultiByteToWideChar(CP_UTF8, 0, &outputDataStr[0], (int)outputDataStr.size(), NULL, 0);
 	std::wstring dataWstr(size_needed, 0);
-	MultiByteToWideChar(CP_UTF8, 0, &dataStr[0], (int)dataStr.size(), &dataWstr[0], size_needed);
+	MultiByteToWideChar(CP_UTF8, 0, &outputDataStr[0], (int)outputDataStr.size(), &dataWstr[0], size_needed);
 	return dataWstr;
 }
 
 
-String^ s2ps(const std::string& dataStr)
+String^ s2ps(const std::string& outputDataStr)
 {
-	std::wstring dataWstr = s2ws(dataStr);
+	std::wstring dataWstr = s2ws(outputDataStr);
 	String^ dataPstr = ref new String(dataWstr.c_str());
 	return dataPstr;
 }
@@ -63,8 +63,8 @@ std::string ws2s(const std::wstring& dataWstr)
 std::string ps2s(String^& dataPstr)
 {
 	std::wstring dataPstrWstr(dataPstr->Data());
-	auto dataStr = ws2s(dataPstrWstr);
-	return dataStr;
+	auto outputDataStr = ws2s(dataPstrWstr);
+	return outputDataStr;
 }
 
 
@@ -98,15 +98,163 @@ String^ i2ps(const int& dataInt)
 }
 
 
+// Main client Socket
+NetSocket socketListener;
+// Check connection with server
+NetResult ifConnected = NetResult::Net_NotYetImplemented;
+// Check number of messages
+int numberOfMessage = 0;
+
+
+void PowerSplitClient::MainPage::PageLoaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	// Clearing server output area
+	textBlockInfoOutput->Text = "";
+	// Clearing computing output area
+	textBlockComputingOutput->Text = "";
+}
+
+
 void PowerSplitClient::MainPage::ConnectButtonClick(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
+	if (Network::Initialize()) {
+		std::string outputDataStr = "WinSock API successfully initialized\r\n";
+		textBlockInfoOutput->Text += s2ps(outputDataStr);
 
+		if (socketListener.Create() == NetResult::Net_Success)
+		{
+			std::string outputDataStr = "Socket successfully created\r\n";
+			textBlockInfoOutput->Text += s2ps(outputDataStr);
+
+			String^ ipPstr = hostTextBox->Text;
+			std::string ipStr = ps2s(ipPstr);
+			const char* ipChar = ipStr.c_str();
+
+			String^ portPstr = portTextBox->Text;
+			int portInt = ps2i(portPstr);
+
+			if (socketListener.Connect(IPEndpoint(ipChar, portInt)) == NetResult::Net_Success)
+			{
+				ifConnected = NetResult::Net_Success;
+
+				std::string outputDataStr = "Succesfully connected to server!\r\n";
+				textBlockInfoOutput->Text += s2ps(outputDataStr);
+
+				// Prepare sending data to check connection
+				uint32_t messageNumberToSend, messageSizeToSend;
+				std::string messageDataToSend;
+				messageDataToSend = "Hello, Server!";
+				messageNumberToSend = ++numberOfMessage;
+				messageSizeToSend = messageDataToSend.size();
+				NetPacket packetToSend;
+				packetToSend << messageNumberToSend << messageSizeToSend << messageDataToSend;
+
+				// Send the prepared packet with data
+				NetResult resultSent = NetResult::Net_Success;
+				while (resultSent == NetResult::Net_Success)
+				{
+					std::string outputDataStr = "Attempting to send set of data...\r\n";
+					textBlockInfoOutput->Text += s2ps(outputDataStr);
+
+					resultSent = socketListener.Send(packetToSend);
+					if (resultSent != NetResult::Net_Success)
+						break;
+
+					outputDataStr = "Data has been sent to server\r\n";
+					textBlockInfoOutput->Text += s2ps(outputDataStr);
+					break;
+				}
+
+				// Wait for response data from server
+				uint32_t messageNumberReceived, messageSizeReceived;
+				std::string messageDataReceived;
+				NetPacket packetReceived;
+				NetResult resultReceived = NetResult::Net_Success;
+				while (resultReceived == NetResult::Net_Success)
+				{
+					resultReceived = socketListener.Receive(packetReceived);
+					if (resultReceived != NetResult::Net_Success)
+						break;
+
+					std::string outputDataStr = "Data received:\r\n";
+					textBlockInfoOutput->Text += s2ps(outputDataStr);
+					try
+					{
+						packetReceived >> messageNumberReceived >> messageSizeReceived >> messageDataReceived;
+					}
+					catch (NetPacketException& exception)
+					{
+						std::string outputDataStr = exception.ToString() + "\r\n";
+						textBlockInfoOutput->Text += s2ps(outputDataStr);
+					}
+					outputDataStr = messageDataReceived + "\r\n";
+					textBlockInfoOutput->Text += s2ps(outputDataStr);
+					break;
+				}
+
+				outputDataStr = "Data has just been received from the server\r\n";
+				textBlockInfoOutput->Text += s2ps(outputDataStr);
+			}
+			else
+			{
+				std::string outputDataStr = "Failed to connect to server!\r\n";
+				textBlockInfoOutput->Text += s2ps(outputDataStr);
+
+				socketListener.Close();
+
+				ifConnected = NetResult::Net_NotYetImplemented;
+
+				outputDataStr = "Socket successfully closed\r\n";
+				textBlockInfoOutput->Text += s2ps(outputDataStr);
+			}
+		}
+		else
+		{
+			std::string outputDataStr = "Socket failed to create\r\n";
+			textBlockInfoOutput->Text += s2ps(outputDataStr);
+
+			socketListener.Close();
+
+			ifConnected = NetResult::Net_NotYetImplemented;
+
+			outputDataStr = "Socket successfully closed\r\n";
+			textBlockInfoOutput->Text += s2ps(outputDataStr);
+		}
+	}
+	else {
+		std::string outputDataStr = "WSAStartup failed with error\r\n";
+		textBlockInfoOutput->Text += s2ps(outputDataStr);
+
+		Network::Shutdown();
+
+		outputDataStr = "WinSock API successfully closed\r\n";
+		textBlockInfoOutput->Text += s2ps(outputDataStr);
+	}
 }
 
 
 void PowerSplitClient::MainPage::DisconnectButtonClick(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
+	// Check active connection with server
+	if (ifConnected == NetResult::Net_NotYetImplemented)
+	{
+		std::string outputDataStr = "Please first connect to server\r\n";
+		textBlockInfoOutput->Text += s2ps(outputDataStr);
+	}
+	else
+	{
+		socketListener.Close();
 
+		ifConnected = NetResult::Net_NotYetImplemented;
+
+		std::string outputDataStr = "Socket successfully closed\r\n";
+		textBlockInfoOutput->Text += s2ps(outputDataStr);
+
+		Network::Shutdown();
+
+		outputDataStr = "WinSock API successfully closed\r\n";
+		textBlockInfoOutput->Text += s2ps(outputDataStr);
+	}
 }
 
 
@@ -129,12 +277,6 @@ void PowerSplitClient::MainPage::StartTask3Click(Platform::Object^ sender, Windo
 
 
 void PowerSplitClient::MainPage::StartTask4Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
-{
-
-}
-
-
-void PowerSplitClient::MainPage::PageLoaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
 
 }
--- power-split-6-server-cli/power-split-6-server-cli.cpp
+++ power-split-6-server-cli/power-split-6-server-cli.cpp
@@ -15,9 +15,139 @@
 
 using namespace PowerSplitNet;
 
+// Check number of messages
+int numberOfMessage = 0;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	if (Network::Initialize()) {
+		std::string outputDataStr = "WinSock API successfully initialized";
+		std::cout << outputDataStr << std::endl;
+
+		NetSocket socketListener;
+		if (socketListener.Create() == NetResult::Net_Success)
+		{
+			std::string outputDataStr = "Socket successfully created";
+			std::cout << outputDataStr << std::endl;
+
+			if (socketListener.Listen(IPEndpoint("0.0.0.0", 80)) == NetResult::Net_Success) // 0.0.0.0 means that access can be obtained form any IP
+			{
+				std::string outputDataStr = "Socket successfully listening on port 80";
+				std::cout << outputDataStr << std::endl;
+
+				NetSocket socketConnected;
+				if (socketListener.Accept(socketConnected) == NetResult::Net_Success)
+				{
+					std::string outputDataStr = "New connection accepted";
+					std::cout << outputDataStr << std::endl;
+
+					uint32_t messageNumberReceived, messageSizeReceived;
+					std::string messageDataReceived;
+					NetPacket packetReceived;
+
+					uint32_t messageNumberToSend, messageSizeToSend;
+					std::string messageDataToSend;
+					NetPacket packetToSend;
+					
+					NetResult resultReceived = NetResult::Net_Success;
+					while (resultReceived == NetResult::Net_Success)
+					{
+						resultReceived = socketConnected.Receive(packetReceived);
+						if (resultReceived != NetResult::Net_Success)
+							break;
+						else
+						{
+							std::cout << "Data received: ";
+							try
+							{
+								packetReceived >> messageNumberReceived >> messageSizeReceived;
+							}
+							catch (NetPacketException& exception)
+							{
+								std::cout << exception.CStr() << std::endl;
+							}
+
+							if (messageNumberReceived <= 1) {
+								try
+								{
+									packetReceived >> messageDataReceived;
+								}
+								catch (NetPacketException& exception)
+								{
+									std::cout << exception.CStr() << std::endl;
+								}
+
+								std::string outputDataStr = messageDataReceived;
+								std::cout << outputDataStr << std::endl;
+
+								messageDataToSend = "Hello, Client!";
+								messageNumberToSend = ++numberOfMessage;
+								messageSizeToSend = messageDataToSend.size();
+								packetToSend << messageNumberToSend << messageSizeToSend << messageDataToSend;
+
+							}
+							else {
+								try
+								{
+									packetReceived >> messageDataReceived;
+								}
+								catch (NetPacketException& exception)
+								{
+									std::cout << exception.CStr() << std::endl;
+								}
+							}
+
+							NetResult resultSent = NetResult::Net_Success;
+							while (resultSent == NetResult::Net_Success)
+							{
+								std::string outputDataStr = "Attempting to send set of data...";
+								std::cout << outputDataStr << std::endl;
+
+								resultSent = socketConnected.Send(packetToSend);
+								if (resultSent != NetResult::Net_Success)
+									break;
+
+								outputDataStr = "Data has been sent to client";
+								std::cout << outputDataStr << std::endl;
+								break;
+							}
+						}
+					}
+
+					/*socketConnected.Close();
+					outputDataStr = "NewConnectionSocket successfully closed";
+					std::cout << outputDataStr << std::endl;*/
+				}
+				else
+				{
+					std::string outputDataStr = "Failed to accept new connection";
+					std::cout << outputDataStr << std::endl;
+				}
+			}
+			else
+			{
+				std::string outputDataStr = "Failed to listen socket on port 80";
+				std::cout << outputDataStr << std::endl;
+			}
+
+			socketListener.Close();
+			outputDataStr = "Socket successfully closed";
+			std::cout << outputDataStr << std::endl;
+		}
+		else
+		{
+			std::string outputDataStr = "Socket failed to create";
+			std::cout << outputDataStr << std::endl;
+		}
+	}
+	else {
+		std::string outputDataStr = "WSAStartup failed with error";
+		std::cout << outputDataStr << std::endl;
+	}
+
+	Network::Shutdown();
+	std::string outputDataStr = "WinSock API successfully closed";
+	std::cout << outputDataStr << std::endl;
 
 	system("pause");
 	return 0;
